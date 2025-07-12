c5ed9a3595e3cc5e9502de58dc66afee425d01c5
Dommaraju-Tejesh <d.tejeshraju67@gmail.com>
2025-07-12 21:14:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let darkMode = false;
 
+function toggleTheme() {
+  if (!darkMode) {
+    document.documentElement.style.setProperty('--bg-color', '#121212');
+    document.documentElement.style.setProperty('--text-color', '#ffffff');
+    document.documentElement.style.setProperty('--card-bg', '#1e1e1e');
+  } else {
+    document.documentElement.style.setProperty('--bg-color', '#ffffff');
+    document.documentElement.style.setProperty('--text-color', '#000000');
+    document.documentElement.style.setProperty('--card-bg', '#f5f5f5');
+  }
+  darkMode = !darkMode;
+}
